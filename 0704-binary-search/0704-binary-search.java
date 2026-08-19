3b29ee301103ffdc8ef144e6fa2835c8bcefd023
Samrat Chatterjee <samrat9474457@gmail.com>
2026-08-19 20:14:34 +0530

    Time: 0 ms (100%), Space: 48.2 MB (85.76%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+          
+          int mid=0,low=0,high=nums.length-1;
+   
+while(low<=high){
+
+    mid=low+(high-low)/2;
+
+  if(nums[mid]==target){
+      return mid;
+      }
+   else if(nums[mid]<target){
+       low=mid+1;
+        }
+    else{
+        high=mid-1;
+     }
+ 
+ }
+return -1;
+}}
+
